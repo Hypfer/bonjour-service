113d63c3a07f739001198545d2a9c1043e9a5b0b
Sören Beye <github@hypfer.de>
2021-08-17 21:35:04 +0200

    Commit dist folder for easy use

---
+++ dist/lib/browser.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="node" />
+import EventEmitter from 'events';
+export interface BrowserConfig {
+    type: string;
+    protocol?: 'tcp' | 'udp';
+    subtypes?: Array<string>;
+    txt?: any;
+}
+export declare class Browser extends EventEmitter {
+    private mdns;
+    private onresponse;
+    private serviceMap;
+    private txt;
+    private name?;
+    private wildcard;
+    private services;
+    constructor(mdns: any, opts: any, onup?: (...args: any[]) => void);
+    start(): void;
+    stop(): void;
+    update(): void;
+    private addService;
+    private removeService;
+    private goodbyes;
+    private buildServicesFor;
+}
+export default Browser;
